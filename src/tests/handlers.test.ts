b713a42fbcf888b7d8b48a9595a3d7d60ec310f9
Matic Zavadlal <matic.zavadlal@gmail.com>
2018-12-04 12:37:23 +0100

    coverage, missing tests passing

--- src/tests/handlers.test.ts
+++ src/tests/handlers.test.ts
@@ -7,10 +7,6 @@ describe('Sync handler', () => {
   })
 
   test('correctly performs dry run', async () => {
-    expect(2).toBe(2)
-  })
-
-  test('handle', async () => {
     /**
      * Mocks
      */
@@ -22,20 +18,19 @@ describe('Sync handler', () => {
       labels,
       'getRepositoryFromName',
     )
-    const getRepositoryLabelsMock = jest.spyOn(labels, 'getRepostioryLabels')
+    const getRepositoryLabelsMock = jest
+      .spyOn(labels, 'getRepostioryLabels')
+      .mockResolvedValue([])
     const getLabelsDiffMock = jest.spyOn(labels, 'getLabelsDiff')
-    const addLabelsToRepositoryMock = jest.spyOn(
-      labels,
-      'addLabelsToRepository',
-    )
-    const updateLabelsInRepositoryMock = jest.spyOn(
-      labels,
-      'updateLabelsInRepository',
-    )
-    const removeLabelsFromRepositoryMock = jest.spyOn(
-      labels,
-      'removeLabelsFromRepository',
-    )
+    const addLabelsToRepositoryMock = jest
+      .spyOn(labels, 'addLabelsToRepository')
+      .mockImplementation(labels => Promise.resolve(labels))
+    const updateLabelsInRepositoryMock = jest
+      .spyOn(labels, 'updateLabelsInRepository')
+      .mockImplementation(labels => Promise.resolve(labels))
+    const removeLabelsFromRepositoryMock = jest
+      .spyOn(labels, 'removeLabelsFromRepository')
+      .mockImplementation(labels => Promise.resolve(labels))
 
     /**
      * Execution
@@ -49,6 +44,15 @@ describe('Sync handler', () => {
           },
         },
       },
+      'prisma/prisma-binding': {
+        labels: {
+          test: {
+            description: 'Testing sync.',
+            color: '#123456',
+          },
+        },
+        strict: true,
+      },
       'wrong-name': {
         labels: {
           test: {
@@ -60,7 +64,102 @@ describe('Sync handler', () => {
     }
 
     const options: SyncOptions = {
-      githubToken: '',
+      githubToken: 'token',
+      dryRun: true,
+    }
+
+    const res = await handleSync(configuration, options)
+
+    /**
+     * Tests
+     */
+
+    expect(getRepositoriesFromConfigurationMock).toHaveBeenCalledTimes(1)
+    expect(getRepositoryFromNameMock).toHaveBeenCalledTimes(3)
+    expect(getRepositoryLabelsMock).toHaveBeenCalledTimes(1)
+    expect(getLabelsDiffMock).toHaveBeenCalledTimes(1)
+    expect(addLabelsToRepositoryMock).toHaveBeenCalledTimes(0)
+    expect(updateLabelsInRepositoryMock).toHaveBeenCalledTimes(0)
+    expect(removeLabelsFromRepositoryMock).toHaveBeenCalledTimes(0)
+    expect(res).toEqual({
+      config: configuration,
+      options: options,
+      successes: [],
+      errors: [],
+    } as SyncReport)
+
+    /**
+     * Clearings
+     */
+
+    getRepositoriesFromConfigurationMock.mockRestore()
+    getRepositoryFromNameMock.mockRestore()
+    getRepositoryLabelsMock.mockRestore()
+    getLabelsDiffMock.mockRestore()
+    addLabelsToRepositoryMock.mockRestore()
+    updateLabelsInRepositoryMock.mockRestore()
+    removeLabelsFromRepositoryMock.mockRestore()
+  })
+
+  test('correctly handles sync', async () => {
+    /**
+     * Mocks
+     */
+    const getRepositoriesFromConfigurationMock = jest.spyOn(
+      labels,
+      'getRepositoriesFromConfiguration',
+    )
+    const getRepositoryFromNameMock = jest.spyOn(
+      labels,
+      'getRepositoryFromName',
+    )
+    const getRepositoryLabelsMock = jest
+      .spyOn(labels, 'getRepostioryLabels')
+      .mockResolvedValue([])
+    const getLabelsDiffMock = jest.spyOn(labels, 'getLabelsDiff')
+    const addLabelsToRepositoryMock = jest
+      .spyOn(labels, 'addLabelsToRepository')
+      .mockImplementation(labels => Promise.resolve(labels))
+    const updateLabelsInRepositoryMock = jest
+      .spyOn(labels, 'updateLabelsInRepository')
+      .mockImplementation(labels => Promise.resolve(labels))
+    const removeLabelsFromRepositoryMock = jest
+      .spyOn(labels, 'removeLabelsFromRepository')
+      .mockImplementation(labels => Promise.resolve(labels))
+
+    /**
+     * Execution
+     */
+    const configuration: Config = {
+      'prisma/github-labels': {
+        labels: {
+          test: {
+            description: 'Testing sync.',
+            color: '#123456',
+          },
+        },
+      },
+      'prisma/prisma-binding': {
+        labels: {
+          test: {
+            description: 'Testing sync.',
+            color: '#123456',
+          },
+        },
+        strict: true,
+      },
+      'wrong-name': {
+        labels: {
+          test: {
+            description: 'Testing sync.',
+            color: '#123456',
+          },
+        },
+      },
+    }
+
+    const options: SyncOptions = {
+      githubToken: 'token',
       dryRun: false,
     }
 
@@ -71,12 +170,109 @@ describe('Sync handler', () => {
      */
 
     expect(getRepositoriesFromConfigurationMock).toHaveBeenCalledTimes(1)
-    expect(getRepositoryFromNameMock).toHaveBeenCalledTimes(2)
-    expect(getRepositoryLabelsMock).toHaveBeenCalledTimes(1)
-    expect(getLabelsDiffMock).toHaveBeenCalledTimes(1)
-    expect(addLabelsToRepositoryMock).toHaveBeenCalledTimes(0)
-    expect(updateLabelsInRepositoryMock).toHaveBeenCalledTimes(0)
-    expect(removeLabelsFromRepositoryMock).toHaveBeenCalledTimes(0)
+    expect(getRepositoryFromNameMock).toHaveBeenCalledTimes(3)
+    expect(getRepositoryLabelsMock).toHaveBeenCalledTimes(2)
+    expect(getLabelsDiffMock).toHaveBeenCalledTimes(2)
+    expect(addLabelsToRepositoryMock).toHaveBeenCalledTimes(2)
+    expect(updateLabelsInRepositoryMock).toHaveBeenCalledTimes(2)
+    expect(removeLabelsFromRepositoryMock).toHaveBeenCalledTimes(2)
+    expect(res).toEqual({
+      config: configuration,
+      options: options,
+      successes: [],
+      errors: [],
+    } as SyncReport)
+
+    /**
+     * Clearings
+     */
+
+    getRepositoriesFromConfigurationMock.mockRestore()
+    getRepositoryFromNameMock.mockRestore()
+    getRepositoryLabelsMock.mockRestore()
+    getLabelsDiffMock.mockRestore()
+    addLabelsToRepositoryMock.mockRestore()
+    updateLabelsInRepositoryMock.mockRestore()
+    removeLabelsFromRepositoryMock.mockRestore()
+  })
+
+  test('correctly handles errors in repository sync', async () => {
+    /**
+     * Mocks
+     */
+    const getRepositoriesFromConfigurationMock = jest.spyOn(
+      labels,
+      'getRepositoriesFromConfiguration',
+    )
+    const getRepositoryFromNameMock = jest.spyOn(
+      labels,
+      'getRepositoryFromName',
+    )
+    const getRepositoryLabelsMock = jest
+      .spyOn(labels, 'getRepostioryLabels')
+      .mockResolvedValue([])
+    const getLabelsDiffMock = jest.spyOn(labels, 'getLabelsDiff')
+    const addLabelsToRepositoryMock = jest
+      .spyOn(labels, 'addLabelsToRepository')
+      .mockImplementation(labels => Promise.resolve(labels))
+    const updateLabelsInRepositoryMock = jest
+      .spyOn(labels, 'updateLabelsInRepository')
+      .mockImplementation(labels => Promise.resolve(labels))
+    const removeLabelsFromRepositoryMock = jest
+      .spyOn(labels, 'removeLabelsFromRepository')
+      .mockImplementation(() => {
+        throw new Error()
+      })
+
+    /**
+     * Execution
+     */
+    const configuration: Config = {
+      'prisma/github-labels': {
+        labels: {
+          test: {
+            description: 'Testing sync.',
+            color: '#123456',
+          },
+        },
+      },
+      'prisma/prisma-binding': {
+        labels: {
+          test: {
+            description: 'Testing sync.',
+            color: '#123456',
+          },
+        },
+        strict: true,
+      },
+      'wrong-name': {
+        labels: {
+          test: {
+            description: 'Testing sync.',
+            color: '#123456',
+          },
+        },
+      },
+    }
+
+    const options: SyncOptions = {
+      githubToken: 'token',
+      dryRun: false,
+    }
+
+    const res = await handleSync(configuration, options)
+
+    /**
+     * Tests
+     */
+
+    expect(getRepositoriesFromConfigurationMock).toHaveBeenCalledTimes(1)
+    expect(getRepositoryFromNameMock).toHaveBeenCalledTimes(3)
+    expect(getRepositoryLabelsMock).toHaveBeenCalledTimes(2)
+    expect(getLabelsDiffMock).toHaveBeenCalledTimes(2)
+    expect(addLabelsToRepositoryMock).toHaveBeenCalledTimes(2)
+    expect(updateLabelsInRepositoryMock).toHaveBeenCalledTimes(2)
+    expect(removeLabelsFromRepositoryMock).toHaveBeenCalledTimes(2)
     expect(res).toEqual({
       config: configuration,
       options: options,
