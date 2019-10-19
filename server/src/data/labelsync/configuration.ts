52b4245df391e22517c2ef0762605d974064335f
maticzav <matic.zavadlal@gmail.com>
2019-10-19 21:41:14 +0200

    validation

--- server/src/data/labelsync/configuration.ts
+++ server/src/data/labelsync/configuration.ts
@@ -1,11 +1,12 @@
+import * as a from 'fp-ts/lib/Array'
 import * as e from 'fp-ts/lib/Either'
+import * as o from 'fp-ts/lib/Option'
 import { Task } from 'fp-ts/lib/Task'
 import * as t from 'io-ts'
 import { Octokit } from 'probot'
 import yaml from 'yaml'
 
 import { labelSyncConfigurationFilePath } from '../constants'
-import { identity } from 'fp-ts/lib/function'
 
 /**
  * Every type that is related to configuration.
@@ -78,35 +79,11 @@ const LSRepositoryName = t.string
 export type LSRepositoryName = t.TypeOf<typeof LSRepositoryName>
 
 /**
- * Represents a repository which is closed on itself.
- *
- * Checks:
- *  * every sibling has a repository definition.
- */
-interface LSWellConfiguredRepositoryBrand {
-  readonly LSWellConfiguredRepository: unique symbol
-}
-const LSWellConfiguredRepository = t.brand(
-  LSRepository, // a codec representing the original type
-  (repo): repo is t.Branded<LSRepository, LSWellConfiguredRepositoryBrand> => {
-    const labelsNames = Object.keys(repo.labels)
-    /* Check whether repository configuration defines all siblings. */
-    return labelsNames.some(labelName => {
-      const label = repo.labels[labelName]!
-      return label.siblings.some(sibling =>
-        labelsNames.some(label => label !== sibling),
-      )
-    })
-  },
-  'LSWellConfiguredRepository',
-)
-
-/**
  * Configuration represents an entire configuration for all
  * LabelSync tools that an organisation is using.
  */
 const LSConfiguration = t.type({
-  repos: t.record(LSRepositoryName, LSWellConfiguredRepository),
+  repos: t.record(LSRepositoryName, LSRepository),
 })
 type LSConfiguration = t.TypeOf<typeof LSConfiguration>
 export interface ILSConfiguration extends LSConfiguration {}
@@ -114,14 +91,41 @@ export interface ILSConfiguration extends LSConfiguration {}
 /* Validation */
 
 export type LSConfigurationError =
+  | LSConfigurationLocationError
+  | LSConfigurationShapeError
+  | LSConfigurationContentError
+
+/* Problems with configuration file location on Github. */
+export type LSConfigurationLocationError =
   | {
-      type: 'LOAD'
-      message: string
+      type: 'LOCATION'
+      context: Octokit.Response<Octokit.ReposGetContentsResponse>
+      kind: 'FILE_TYPE' | 'CONNECTION'
+      message?: string
     }
   | {
-      type: 'VALIDATION'
-      message: string[]
+      type: 'LOCATION'
+      kind: 'OTHER'
+      error: Error
     }
+
+/* Wrong file structure. */
+export type LSConfigurationShapeError = {
+  type: 'SHAPE'
+  fields: Array<{
+    path: string[]
+    message?: string
+  }>
+}
+
+/* Problems with configuration content. */
+export type LSConfigurationContentError = {
+  type: 'CONTENT'
+  missing: Array<{
+    path: string[]
+    siblings: string[]
+  }>
+}
 
 /**
  * Processes a decoded YAML configuration. It doesn't perform
@@ -132,7 +136,7 @@ export type LSConfigurationError =
 export function validateYAMLConfiguration(
   yaml: object,
 ): e.Either<LSConfigurationError, LSConfiguration> {
-  return e.mapLeft(validationErrorToLSConfigurationError)(
+  return e.mapLeft(validationErrorToLSConfigurationShapeError)(
     LSConfiguration.decode(yaml),
   )
 }
@@ -142,24 +146,73 @@ export function validateYAMLConfiguration(
  *
  * @param errors
  */
-function validationErrorToLSConfigurationError(
+function validationErrorToLSConfigurationShapeError(
   errors: Array<t.ValidationError>,
 ): LSConfigurationError {
-  return { type: 'VALIDATION', message: [] }
+  return { type: 'SHAPE', fields: errors.map(getError) }
+
+  function getError(
+    e: t.ValidationError,
+  ): { path: string[]; message?: string } {
+    return { path: e.context.map(c => c.key), message: e.message }
+  }
 }
 
-/* Github related stuff. */
+/**
+ * Validates configuration contents.
+ *
+ * @param config
+ */
+export function validateConfigurationContents(
+  config: LSConfiguration,
+): e.Either<LSConfigurationError, LSConfiguration> {
+  /* Helper functions */
+  const repos = Object.keys(config.repos)
+  const repo = (name: string): LSRepository => config.repos[name]!
+
+  /* Validator */
+  const errors = a.reduce<
+    string,
+    Array<{ path: string[]; siblings: string[] }>
+  >([], (acc, repoName) => {
+    const repoConfig = repo(repoName)
+    const labels = Object.keys(repoConfig.labels)
+    const label = (name: string): LSLabel => repoConfig.labels[name]!
+
+    /* Check whether repository configuration defines all siblings. */
+    const labelsWithMissingSiblings = a.filterMap<
+      string,
+      { path: string[]; siblings: string[] }
+    >(labelName => {
+      const labelConfig = label(labelName)
+      const missingSiblings = labelConfig.siblings.filter(sibling =>
+        labels.some(label => label === sibling),
+      )
+
+      if (missingSiblings.length > 0) {
+        return o.some({
+          path: [repoName, labelName],
+          siblings: missingSiblings,
+        })
+      } else {
+        return o.none
+      }
+    })(labels)
+
+    return acc.concat(labelsWithMissingSiblings)
+  })(repos)
+
+  if (errors.length > 0) {
+    return e.left({ type: 'CONTENT', missing: errors })
+  }
+
+  return e.right(config)
+}
 
 export interface LSConfigurationParams {
   owner: string
   repo: string
   ref: string
-}
-
-export interface LoadYamlConfigFileError {
-  type: 'FILE_TYPE' | 'CONNECTION' | 'OTHER'
-  context: Octokit.Response<Octokit.ReposGetContentsResponse>
-  message?: string
 }
 
 /**
@@ -171,29 +224,33 @@ export interface LoadYamlConfigFileError {
 export const loadYAMLConfigFile = (
   octokit: Octokit,
   { owner, repo, ref }: LSConfigurationParams,
-): Task<e.Either<LoadYamlConfigFileError, any>> => async () => {
-  const res = await octokit.repos.getContents({
-    owner: owner,
-    path: labelSyncConfigurationFilePath,
-    repo: repo,
-    ref: ref,
-  })
+): Task<e.Either<LSConfigurationLocationError, any>> => async () => {
+  try {
+    const res = await octokit.repos.getContents({
+      owner: owner,
+      path: labelSyncConfigurationFilePath,
+      repo: repo,
+      ref: ref,
+    })
 
-  switch (res.status) {
-    case 200: {
-      /* Validate configuration file. */
-      if (Array.isArray(res.data) || !res.data.content) {
-        return e.left({ type: 'FILE_TYPE', context: res })
-      } else {
-        const buffer = Buffer.from(res.data.content, 'base64').toString()
-        const yamlConfig = yaml.parse(buffer, {})
+    switch (res.status) {
+      case 200: {
+        /* Validate configuration file. */
+        if (Array.isArray(res.data) || !res.data.content) {
+          return e.left({ type: 'LOCATION', context: res, kind: 'FILE_TYPE' })
+        } else {
+          const buffer = Buffer.from(res.data.content, 'base64').toString()
+          const yamlConfig = yaml.parse(buffer, {})
 
-        return e.right(yamlConfig)
+          return e.right(yamlConfig)
+        }
+      }
+      default: {
+        /* Process the error. */
+        return e.left({ type: 'LOCATION', kind: 'CONNECTION', context: res })
       }
     }
-    default: {
-      /* Process the error. */
-      return e.left({ type: 'CONNECTION', context: res })
-    }
+  } catch (err) {
+    return e.left({ type: 'LOCATION', kind: 'OTHER', error: err })
   }
 }
