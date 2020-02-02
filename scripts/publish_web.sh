4e07b8c342f2709dc96a42aca1342af031e2b2be
maticzav <matic.zavadlal@gmail.com>
2020-02-02 13:04:24 +0100

    feat: seo

--- scripts/publish_web.sh
+++ scripts/publish_web.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Clear 
 rm -rf public
 
+# Build webpage.
 cd web
 yarn run build
 
 cd ../
+
+# Expose webpage
 mkdir public
 cp -R ./web/public/* ./public/
+
+# Expose assets
+mkdir public/assets
+cp -R ./assets/* ./public/assets/
