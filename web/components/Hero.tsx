64d07f8916c7b4ce21e895e18b527b7d932122d9
maticzav <matic.zavadlal@gmail.com>
2020-05-11 12:15:51 +0200

    fix: typo on website

--- web/components/Hero.tsx
+++ web/components/Hero.tsx
@@ -10,7 +10,7 @@ export default function Hero() {
         <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
           Sync labels.
           <br className="xl:hidden" />
-          <span className="text-green-600">accross all repositories.</span>
+          <span className="text-green-600">across all repositories.</span>
         </h2>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           Managing Github labels across multiple repositories is hard.
