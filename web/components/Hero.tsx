6908446bc4a4f2f89f71ad90ed8c6f8086a2976e
maticzav <matic.zavadlal@gmail.com>
2020-05-11 12:09:36 +0200

    fix: typo on website

--- web/components/Hero.tsx
+++ web/components/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
           <span className="text-green-600">accross all repositories.</span>
         </h2>
         <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-          Managing Github labels accross multiple repositories is hard.
+          Managing Github labels across multiple repositories is hard.
           <br className="hidden md:inline-block" />
           LabelSync makes it <span className="underline-green">easy</span>.
         </p>
