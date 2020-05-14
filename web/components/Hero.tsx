9e7e492d07b6f36bfa8df34c92e8687ffab1cc65
maticzav <matic.zavadlal@gmail.com>
2020-05-14 22:13:51 +0200

    fix linebreak on phone

--- web/components/Hero.tsx
+++ web/components/Hero.tsx
@@ -17,16 +17,20 @@ export default function Hero() {
     <div className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
       <div className="text-center">
         {/* Hero */}
-        <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-          {/* Typing animation */}
-          <Typing
-            index={typingIndex}
-            onTypingDone={nextTyping}
-            key={typingIndex}
-          ></Typing>
-          {/* End of typing animation */}
-        </h2>
-        <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+        <div className="h-32">
+          <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+            {/* Typing animation */}
+            <Typing
+              index={typingIndex}
+              onTypingDone={nextTyping}
+              key={typingIndex}
+            ></Typing>
+            {/* End of typing animation */}
+          </h2>
+        </div>
+
+        {/* Explanation */}
+        <p className="mt-5 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-10 md:text-xl md:max-w-3xl">
           Managing Github labels across multiple repositories is hard.
           <br className="hidden md:inline-block" />
           LabelSync makes it <span className="underline-green">easy</span>.
@@ -76,8 +80,8 @@ export default function Hero() {
 const typings = shuffle([
   {
     lead: 'Sync labels.',
-    sub: 'across all repositories.',
-    color: 'text-green-400',
+    sub: 'across all\n repositories.',
+    color: 'text-green-400 whitespace-pre md:whitespace-normal',
   },
   {
     lead: 'Your Tasks and Code.',
