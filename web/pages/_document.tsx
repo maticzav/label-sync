ce50bdb176be60c28b58b6875778ece5e0c6b87c
maticzav <matic.zavadlal@gmail.com>
2021-08-19 15:38:45 +0200

    :sparkles: add crisp to the website

--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -52,6 +52,14 @@ export default class extends Document {
 
           {/* Splitbee */}
           <script async src="https://cdn.splitbee.io/sb.js" />
+
+          {/* Crisp */}
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="f485b5a2-8a24-43ec-8783-7542d6ef3c25";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
+            }}
+          />
         </Head>
 
         {/* App */}
